580a10a7e22784fd322689c6790f225094fdfbb4
Axel Axelsson <Axel92_x@hotmail.com>
2019-11-06 17:33:14 +0100

    view and navbar created

---
+++ Pawsome/Pawsome/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Pawsome
+//
+//  Created by Axel Axelsson on 2019-11-06.
+//  Copyright © 2019 ProjectGroupM. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
