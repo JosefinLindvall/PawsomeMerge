b65b0a4ff55cb2e769833f0b134511c5e942799a
Kim Berg <kim.berg99@hotmail.com>
2019-11-08 12:31:39 +0100

    Working on data transfers using segues

--- Pawsome/Pawsome/NotesViewController.swift
+++ Pawsome/Pawsome/NotesViewController.swift
@@ -13,18 +13,42 @@ class NotesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        if(segue.identifier == "medicalCareSegue"){
+            if let nextVC = segue.destination as? NotesOfDetailedCategoryTableViewController{
+                
+                nextVC.title = "testingMedicalCareSegue"
+                nextVC.date = "7/11 - 2019"
+            }
+        }
+        else if(segue.identifier == "healthCareSegue"){
+            
+        }
+        else if(segue.identifier == "activitiesSegue"){
+            
+        }
+        else if(segue.identifier == "otherSegue"){
+            
+        }
+        
+        
+        
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
